35111c739db052fb0e2f767cc1d5d8484c947300
ying45 <fabiying1@gmail.com>
2020-01-22 09:38:30 +0100

    modification main.c

--- main.c
+++ main.c
@@ -5,7 +5,10 @@ int main(){
   Stack s;
   s=create_stack();
   s=push(s, 7);
-  printf("%d\n", top(s));
+  printf("PUSH : %d\n", top(s));
+  s=push(s, 9);
+  printf("PUSH : %d\n", top(s));
+
   free(s);
 
   return 0;
